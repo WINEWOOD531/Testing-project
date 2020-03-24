10ac49d7908057e61a6181b3ecfb5503031d73c1
ТАРАС Одинюк <taras.odynyuk@gmail.com>
2020-03-24 20:43:45 +0200

    New Error Icon

---
+++ Test USSRStyleerror/Test USSRStyleerror/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_USSRStyleerror
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+       "Вы совершили недопустимую операцию. Файлы будут удалены,компьютер взорван,а вы расстреляны.",
+       "Внимание!",
+       MessageBoxButtons.OK,
+       MessageBoxIcon.Error,
+       MessageBoxDefaultButton.Button1,
+       MessageBoxOptions.DefaultDesktopOnly);
+        }
+    }
+}
